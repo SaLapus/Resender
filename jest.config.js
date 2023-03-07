a029b322012a196a989c2e0ba29cf64b165951e3
SaLapus <onaera@bk.ru>
2023-03-07 19:24:05 +0300

    Update test configs

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+
+  coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ["**/test/**/*.test.js"],
+};
