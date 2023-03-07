d90598e8adff85e00e196f8a728c314b6ee6829d
SaLapus <onaera@bk.ru>
2023-03-07 18:02:52 +0300

    Commenting configs to later refactoring

--- src/test/sender/chapters.test.ts
+++ src/test/sender/chapters.test.ts
@@ -1,45 +1,45 @@
-import env from "dotenv";
-env.config();
+// import env from "dotenv";
+// env.config();
 
-import { getVolume } from "../../modules/api/api";
-import Chapters from "../../modules/sender/chapters";
+// import { getVolume } from "../../modules/api/api";
+// import Chapters from "../../modules/sender/chapters";
 
-console.log = () => {
-  return;
-};
+// console.log = () => {
+//   return;
+// };
 
-describe("Chapters Test", () => {
-  test("Normal chapter", async () => {
-    expect.assertions(2);
+// describe("Chapters Test", () => {
+//   test("Normal chapter", async () => {
+//     expect.assertions(2);
 
-    const { chapters } = await getVolume(1261);
-    const oldness = new Date("2017-11-16T00:50:00Z");
+//     const { chapters } = await getVolume(1261);
+//     const oldness = new Date("2017-11-16T00:50:00Z");
 
-    const sortedChs = new Chapters(chapters);
-    expect(sortedChs.data).toHaveLength(2);
+//     const sortedChs = new Chapters(chapters);
+//     expect(sortedChs.data).toHaveLength(2);
 
-    const filteredChs = sortedChs.filter(oldness.getTime());
-    expect(filteredChs).toHaveLength(1);
-  });
+//     const filteredChs = sortedChs.filter(oldness.getTime());
+//     expect(filteredChs).toHaveLength(1);
+//   });
 
-  test("Without chapters", () => {
-    const oldness = new Date("2017-11-16T00:50:00Z");
+//   test("Without chapters", () => {
+//     const oldness = new Date("2017-11-16T00:50:00Z");
 
-    const sortedChs = new Chapters(undefined);
-    expect(sortedChs.data).toHaveLength(0);
+//     const sortedChs = new Chapters(undefined);
+//     expect(sortedChs.data).toHaveLength(0);
 
-    const filteredChs = sortedChs.filter(oldness.getTime());
-    expect(filteredChs).toHaveLength(0);
-  });
+//     const filteredChs = sortedChs.filter(oldness.getTime());
+//     expect(filteredChs).toHaveLength(0);
+//   });
 
-  test("Without date", async () => {
-    expect.assertions(2);
+//   test("Without date", async () => {
+//     expect.assertions(2);
 
-    const { chapters } = await getVolume(1261);
-    const sortedChs = new Chapters(chapters);
-    expect(sortedChs.data).toHaveLength(2);
+//     const { chapters } = await getVolume(1261);
+//     const sortedChs = new Chapters(chapters);
+//     expect(sortedChs.data).toHaveLength(2);
 
-    const filteredChs = sortedChs.filter(undefined);
-    expect(filteredChs).toHaveLength(1);
-  });
-});
+//     const filteredChs = sortedChs.filter(undefined);
+//     expect(filteredChs).toHaveLength(1);
+//   });
+// });
