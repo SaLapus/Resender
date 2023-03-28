90b0d6e7d31e0453cfb613ebcd93d4be11bbd2b2
SaLapus <onaera@bk.ru>
2023-03-28 15:27:02 +0300

    Debug logs and two client instances fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as Discord from "discord.js";
 import { Events, GatewayIntentBits } from "discord.js";
 import * as dotenv from "dotenv";
 
-import { exec } from "node:child_process";
+import fs from "node:fs/promises";
 
 dotenv.config();
 
@@ -15,79 +15,7 @@ import Listener from "./modules/sender/listener";
 import type { UpdateInfo } from "./modules/sender/update";
 import getTextUpdate from "./modules/sender/update";
 
-const hook: Discord.WebhookClient = new Discord.WebhookClient({
-  id:
-    (process.env["NODE_ENV"] as string) === "production"
-      ? (process.env["HOOK_RURA_ID"] as string)
-      : (process.env["HOOK_CAPTAINHOOK_ID"] as string),
-  token:
-    (process.env["NODE_ENV"] as string) === "production"
-      ? (process.env["HOOK_RURA_TOKEN"] as string)
-      : (process.env["HOOK_CAPTAINHOOK_TOKEN"] as string),
-});
-
-const DB: IJSONStorage = getDB();
-const UpdatesListener = new Listener();
-
-UpdatesListener.on("update", updateHandler);
-UpdatesListener.shedule();
-
-async function updateHandler(updates: APITypes.VolumeUpdates.Content[]): Promise<void> {
-  for (const u of updates) {
-    if (!u) continue;
-
-    const dateFrom = DB.getTime();
-
-    if (!dateFrom) throw new Error("SL ERROR: BAD TIME --- DROP UPDATE");
-
-    const messageContent = await getTextUpdate({
-      projectID: u.projectId,
-      volumeID: u.volumeId,
-      dateFrom,
-    });
-
-    const message = await sendUpdate(messageContent);
-
-    editMessage(message, messageContent);
-
-    DB.setTime(new Date(u.showTime));
-  }
-}
-
-async function sendUpdate(title: UpdateInfo): Promise<Discord.APIMessage> {
-  const { update, coverBuffer } = title;
-
-  const message = {
-    content: update,
-    allowedMentions: {
-      roles: [process.env["ROLE_TO_PING_ID"] as string],
-    },
-  };
-  if (coverBuffer) Object.assign(message, { files: [coverBuffer] });
-
-  console.log("SENDING " + title.info.title);
-
-  return await hook.send(message);
-}
-
-function editMessage(message: Discord.APIMessage, updateInfo: UpdateInfo) {
-  const interval = setInterval(async () => {
-    const { update } = await getTextUpdate({
-      projectID: updateInfo.info.projectID,
-      volumeID: updateInfo.info.volumeID,
-      dateFrom: updateInfo.info.dateFrom,
-    });
-
-    await hook.editMessage(message as unknown as Discord.Message, {
-      content: update,
-      allowedMentions: {
-        roles: [process.env["ROLE_TO_PING_ID"] as string],
-      },
-    });
-  }, 15 * 60 * 1000);
-
-  setTimeout(() => clearInterval(interval), new Date(0).setHours(4, 1));
-}
+const sendMessagesIds = new Set<string>();
 
 const RuRaColor = new Discord.Client({
   intents: [
@@ -99,26 +27,114 @@ const RuRaColor = new Discord.Client({
   ],
 });
 
-RuRaColor.on(Events.ClientReady, () => {
-  let flag = false;
-  exec('git log -1 --pretty=format:"%H - %an, %ad"', (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-    }
-    if (!flag && stdout) {
-      console.log("START RESENDER V.: " + stdout);
+RuRaColor.on(Events.Debug, (log) => {
+  fs.writeFile(
+    "./logs.txt",
+    `${new Date()
+      .toLocaleDateString("ru-ru", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
 
-      process.env["COMMIT_INFO"] = stdout;
-      flag = true;
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        formatMatcher: "basic",
+      })
+      .replaceAll(".", "/")}: ${log}\n`,
+    {
+      encoding: "utf-8",
+      flag: "a",
     }
-    if (stderr) {
-      console.error(stderr);
+  );
+});
+
+RuRaColor.on(Events.ClientReady, async () => {
+  const hook = await RuRaColor.fetchWebhook(
+    (process.env["NODE_ENV"] as string) === "production"
+      ? (process.env["HOOK_RURA_ID"] as string)
+      : (process.env["HOOK_CAPTAINHOOK_ID"] as string),
+    (process.env["NODE_ENV"] as string) === "production"
+      ? (process.env["HOOK_RURA_TOKEN"] as string)
+      : (process.env["HOOK_CAPTAINHOOK_TOKEN"] as string)
+  );
+
+  const DB: IJSONStorage = getDB();
+  const UpdatesListener = new Listener();
+
+  UpdatesListener.on("update", updateHandler);
+  UpdatesListener.shedule();
+
+  async function updateHandler(updates: APITypes.VolumeUpdates.Content[]): Promise<void> {
+    for (const u of updates) {
+      if (!u) continue;
+
+      const dateFrom = DB.getTime();
+
+      if (!dateFrom) throw new Error("SL ERROR: BAD TIME --- DROP UPDATE");
+
+      const messageContent = await getTextUpdate({
+        projectID: u.projectId,
+        volumeID: u.volumeId,
+        dateFrom,
+      });
+
+      const message = await sendUpdate(messageContent);
+
+      sendMessagesIds.add(message.id);
+
+      editMessage(message, messageContent);
+
+      DB.setTime(new Date(u.showTime));
     }
-  });
+  }
+
+  async function sendUpdate(title: UpdateInfo): Promise<Discord.Message<boolean>> {
+    const { update, coverBuffer } = title;
+
+    const message: Discord.WebhookMessageCreateOptions = {
+      content: update,
+      allowedMentions: {
+        roles: [process.env["ROLE_TO_PING_ID"] as string],
+      },
+    };
+
+    if (coverBuffer) message.files = [coverBuffer];
+
+    console.log("SENDING " + title.info.title);
+
+    return await hook.send(message);
+  }
+
+  function editMessage(message: Discord.Message<boolean>, updateInfo: UpdateInfo) {
+    const interval = setInterval(async () => {
+      const { update } = await getTextUpdate({
+        projectID: updateInfo.info.projectID,
+        volumeID: updateInfo.info.volumeID,
+        dateFrom: updateInfo.info.dateFrom,
+      });
+
+      await hook.editMessage(message as unknown as Discord.Message, {
+        content: update,
+        allowedMentions: {
+          roles: [process.env["ROLE_TO_PING_ID"] as string],
+        },
+      });
+    }, 15 * 60 * 1000);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      sendMessagesIds.delete(message.id);
+    }, new Date(0).setHours(4, 1));
+  }
 });
 
 RuRaColor.on(Events.MessageCreate, async (message: Discord.Message) => {
   if (message.channel.id !== "800044270370684958") return;
+
+  setTimeout(() => {
+    if (!sendMessagesIds.has(message.id)) message.delete();
+  }, 5_000);
 
   const emojis = [message.guild?.emojis.cache.get("248177959192494080"), "❤", "🔥"];
 
